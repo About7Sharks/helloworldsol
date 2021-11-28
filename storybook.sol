e51d928d91f796f7067752892e68b8b07b1041c7
Zachary Carlin <zach@Zacharys-MacBook-Pro-2.local>
2021-11-28 16:45:25 -0500

    simple story telling contract

---
+++ storybook.sol
@@ -0,0 +1,23 @@
+// This contract allows a group to tell a story onchain
+pragma solidity ^0.8.0;
+
+contract Story {
+    // Create an empty string array named story
+    string[] story;
+
+    // On inital contract creation push some words to story
+    constructor(){
+        story.push("There");
+        story.push("once");
+    }
+
+    // Add a word to the end of the story array
+    function addWord(string memory word) public {
+        story.push(word);
+    }
+
+    // Return all the words in the story array
+    function read() public view returns (string[] memory) {
+        return story;
+    }
+}
